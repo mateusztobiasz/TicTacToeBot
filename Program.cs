b68bdbc3ac458c9b51aa025a32cf031865ec8295
Mateusz Tobiasz <01161543@pw.edu.pl>
2023-07-07 20:55:37 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using TicTacToeBotNs;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var bot = new TicTacToeBot();
+        int result = bot.MakeMove(new char[3, 3], false, true);
+    }
+}
---
+++ TicTacToeBot.cs
@@ -0,0 +1,31 @@
+﻿namespace TicTacToeBotNs
+{
+    public class TicTacToeBot
+    {
+        public int MakeMove(char[,] board, bool gameOver, bool botTurn)
+        {
+            if (gameOver) return EvaluatePosition(board);
+
+            if (botTurn) // maximizing player
+            {
+                return 1;
+            }
+            else // minimazing player
+            {
+                return -1;
+            }
+        }
+
+        private bool CheckIfGameOver(char[,] board)
+        {
+            return false;
+        }
+
+        private int EvaluatePosition(char[,] board)
+        {
+            return 1;
+        }
+
+
+    }
+}

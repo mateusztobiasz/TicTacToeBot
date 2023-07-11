aaefa97375f3847c48c468edcf13e5ff6190774f
mateusztobiasz <mati11.2001@gmail.com>
2023-07-11 22:48:18 +0200

    First version of MakeMove minmax function

--- TicTacToeBot.cs
+++ TicTacToeBot.cs
@@ -2,18 +2,62 @@
 {
     public class TicTacToeBot
     {
-        public int MakeMove(char[,] board, bool botTurn)
+        public (int, int) LastBestMove { get; private set; }
+
+
+        public int MakeMove(char[,] board, bool botTurn, int depth)
         {
             if (CheckIfGameOver(board)) return EvaluatePosition(board, botTurn);
+            List<(int, int)> blankSpaces = DetermineBlankSpaces(board);
 
             if (botTurn) // maximizing player
             {
-                return 1;
+                int maxVal = int.MinValue;
+                foreach ((int i, int j) blankSpace in blankSpaces)
+                {
+                    board[blankSpace.i, blankSpace.j] = 'O';
+                    int val = MakeMove(board, !botTurn, depth++);
+                    if (val > maxVal)
+                    {
+                        maxVal = val;
+                        if (depth == 0) LastBestMove = blankSpace;
+                    }
+                    board[blankSpace.i, blankSpace.j] = '_';
+                }
+                return maxVal;
             }
             else // minimazing player
             {
-                return -1;
+                int minVal = int.MaxValue;
+                foreach ((int i, int j) blankSpace in blankSpaces)
+                {
+                    board[blankSpace.i, blankSpace.j] = 'X';
+                    int val = MakeMove(board, !botTurn, depth++);
+                    if (val < minVal)
+                    {
+                        minVal = val;
+                        if (depth == 0) LastBestMove = blankSpace;
+                    }
+                    board[blankSpace.i, blankSpace.j] = '_';
+                }
+                return minVal;
             }
+        }
+
+        private List<(int, int)> DetermineBlankSpaces(char[,] board)
+        {
+            List<(int, int)> blankSpaces = new List<(int, int)> ();
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == '_') blankSpaces.Add((i, j));
+                }
+            }
+
+            return blankSpaces;
+
         }
 
         private bool CheckIfGameOver(char[,] board)
@@ -45,7 +89,7 @@
             bool result = horizontalWin || verticalWin || diagonalWin;
 
             if (result && botTurn) return (board[0, 0] == 'O') ? 1 : -1;
-            else if (result && !botTurn) return (board[0, 0] == 'O') ? -1 : 1;
+            else if (result && !botTurn) return (board[0, 0] == 'X') ? 1 : -1;
             else return 0;
         }
 

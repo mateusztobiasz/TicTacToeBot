d9a50b16e82ac5a92a99687f7322cf0a4c0400d9
mateusztobiasz <mati11.2001@gmail.com>
2023-07-11 18:32:02 +0200

    Added position evaluation and game over determination

--- TicTacToeBot.cs
+++ TicTacToeBot.cs
@@ -2,9 +2,9 @@
 {
     public class TicTacToeBot
     {
-        public int MakeMove(char[,] board, bool gameOver, bool botTurn)
+        public int MakeMove(char[,] board, bool botTurn)
         {
-            if (gameOver) return EvaluatePosition(board);
+            if (CheckIfGameOver(board)) return EvaluatePosition(board, botTurn);
 
             if (botTurn) // maximizing player
             {
@@ -18,12 +18,35 @@
 
         private bool CheckIfGameOver(char[,] board)
         {
-            return false;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == '_') return false;
+                }
+            }
+
+            return true;
         }
 
-        private int EvaluatePosition(char[,] board)
+        private int EvaluatePosition(char[,] board, bool botTurn)
         {
-            return 1;
+            bool horizontalWin = (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2]) ||
+                                 (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2]) ||
+                                 (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2]);
+
+            bool verticalWin = (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0]) ||
+                               (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1]) ||
+                               (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2]);
+
+            bool diagonalWin = (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) ||
+                               (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2]);
+
+            bool result = horizontalWin || verticalWin || diagonalWin;
+
+            if (result && botTurn) return (board[0, 0] == 'O') ? 1 : -1;
+            else if (result && !botTurn) return (board[0, 0] == 'O') ? -1 : 1;
+            else return 0;
         }
 
 
